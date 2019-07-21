c7a3c35ac470e6d68f7328c0251cb7f1612e360a
Shannon Ferguson <shannonferguson@Shannons-MacBook-Pro.local>
2019-07-21 13:15:09 +0200

    added link to movie detail page

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import Movie from './Movie';
+import LinkButton from './LinkButton.js'
+import { Route, Link,Switch} from 'react-router-dom';
+import MovieDetail from './MovieDetail/MovieDetail'
 
 
 const MoviesList = ({movies}) => {
+	
 	return(
+	
 		<div>
 		{
 			movies.map((movie, i) => {
 				return(
-			<Movie 
+					<LinkButton key = {[i]} to='./MovieDetail' className='tc bg-black dib br3 pa2 ma2 bw8 shadow-5' >
+		
+					<Movie 
 					key = {movies[i].title}
 					id = {movies[i].title}
 					title ={movie.title}
 					poster_path ={movie.poster_path}
 					/>
+					
+					</LinkButton>
+				
 				);
     		})
 		}
+
+
+
 		</div>
+
 		);
 	}
+
+function  goToDetail(movie, i){
+		console.log(movie);
+
+	}
+		//<Link to="/MoviesInTheatres/MoviesInTheatres" onClick={() => goToDetail(movie, i)}>Here</Link>
+		//<button key = {i} className='tc bg-black dib br3 pa2 ma2 bw8 shadow-5' onClick={() => goToDetail(movie, i)}>
 
 export default MoviesList;
