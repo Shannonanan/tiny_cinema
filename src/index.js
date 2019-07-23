ab6354bcfd6d7248c7e568d02d8e259f20e47d30
Shannon Ferguson <shannonferguson@Shannons-MacBook-Pro.local>
2019-07-23 18:25:53 +0200

    updated react-router

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Toolbar from './components/Toolbar';
 
 
 
+
 ReactDOM.render(<Toolbar />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
