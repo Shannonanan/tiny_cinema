1cadd28911a1aebf4049e45d38b07bb591aefbed
Shannon Ferguson <shannonferguson@Shannons-MacBook-Pro.local>
2019-08-03 20:13:17 +0200

    added search movie functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,7 @@ import Toolbar from '../components/Toolbar';
 import Scroll from '../components/Scroll';
 
 import { Route, Link,Switch, BrowserRouter as Router } from 'react-router-dom';
-import BestMoviesFrom from '../components/BestMoviesFrom/BestMoviesFrom';
+import SearchMovies from '../components/SearchMovies/SearchMovies';
 import MoviesInTheatres from '../components/MoviesInTheatres/MoviesInTheatres';
 import MovieDetail from '../components/MovieDetail/MovieDetail';
 import MovieReviews from '../components/MovieReviews/MovieReviews';
@@ -24,7 +24,7 @@ class App extends Component {
         <Toolbar/>
          <Switch>
      <Route exact={true} path="/components/PopularMovies" component={PopularMovies}/>
-      <Route exact={true} path="/components/BestMoviesFrom" component={BestMoviesFrom}/>
+      <Route exact={true} path="/components/SearchMovies" component={SearchMovies}/>
        <Route exact={true} path="/components/MoviesInTheatres" component={MoviesInTheatres}/>
        <Route exact={true} path="/components/MovieDetail/:movieId" component={MovieDetail} />
        <Route exact={true} path="/components/MovieReviews/:movieId" component={MovieReviews} />
